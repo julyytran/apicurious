cbc4b30378eaac24153a4ed944a71ae6a816a53f
July Tran <julyytran@gmail.com>
2016-04-06 15:39:49 -0600

    user can view feed

--- spec/feature/user_can_view_account_info_spec.rb
+++ spec/feature/user_can_view_account_info_spec.rb
@@ -13,18 +13,13 @@ feature "user can view account info" do
         VCR.use_cassette 'github_service#repos' do
           VCR.use_cassette 'github_service#starred' do
             VCR.use_cassette 'github_service#organizations' do
-              VCR.use_cassette 'github_service#user_pushes' do
-                VCR.use_cassette 'github_service#following_pushes' do
-
-                  click_on "julyytran's Account Info"
-                  expect(page).to have_content "julyytran's Info"
-                  expect(page).to have_content "Followers: 12"
-                  expect(page).to have_content "Following: 1"
-                  expect(page).to have_content "Starred Repos: 1"
-                  expect(page).to have_content "Organizations: 1"
-                  expect(page).to have_content "My Repositories: 30"
-                end
-              end
+              click_on "julyytran's Account Info"
+              expect(page).to have_content "julyytran's Info"
+              expect(page).to have_content "Followers: 12"
+              expect(page).to have_content "Following: 1"
+              expect(page).to have_content "Starred Repos: 1"
+              expect(page).to have_content "Organizations: 1"
+              expect(page).to have_content "My Repositories: 30"
             end
           end
         end
