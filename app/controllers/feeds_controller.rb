fba7f77f82d03c4440a5461800d74bc0bdca2949
July Tran <julyytran@gmail.com>
2016-04-07 15:55:42 -0600

    remove unused code

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,6 +1,4 @@
 class FeedsController < ApplicationController
   def index
-    @user_pushes = PushEvent.user_pushes(current_user)
-    @following_pushes = PushEvent.following_pushes(current_user)
   end
 end
